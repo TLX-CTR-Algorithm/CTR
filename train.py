ee0bea33052b1c2c22538065986b81fb3f8dba12
liuysong <liuyong_song@126.com>
2018-10-12 14:58:38 +0800

    train model

--- train.py
+++ train.py
@@ -1,32 +1,69 @@
-
 import tensorflow as tf
-import model
+import model2
 import config
-import pandas as pd
+import logging
+import sample
+import os
 
 slim = tf.contrib.slim
 
-#需要添加校验checkpoint文件的代码
+#设置日志打印格式
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
+                    datefmt='%a, %d %b %Y %h:%M:%S',
+                    )
+
 #需要考虑添加summary部分代码
 
-def gendata( is_training=True ):
-    #with open(config.train_path,encoding='utf-8') as trainf:
-    #此处代码需要根据输入数据形态进行改进
-    if is_training:
-        with pd.read_csv(config.train_path,encoding='utf-8') as trainf:
-            train_x = trainf[:-2]
-            train_y = trainf[-1]
-            return train_x,train_y
-    else:
-        with pd.read_csv(config.test_path,encoding='utf-8') as testf:
-            test_x = testf[:-1]
-            return test_x
+#模型训练函数
+def train_model(batch_size=config.batch_size):
+    #获取训练数据
+    inputs, lables = sample.gendata(is_training=True)
+
+    #构建网络模型
+    dnnmodel = model2.Model
+    dnnmodel.build()
+
+    #如果没有checkpoint文件则需要对所有变量进行初始化
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        sess.run(tf.local_variables_initializer())
+        logging.debug('Initialized')
+
+    #校验加载ckp文件
+    try:
+        saver = tf.train.Saver(max_to_keep=5)
+        checkpoint_path = tf.train.latest_checkpoint(config.output_dir)
+        saver.restore(sess, checkpoint_path)
+        logging.debug('restore from [{0}]'.format(checkpoint_path))
+    except Exception:
+        logging.debug('nocheck point found...')
+
+    #会按照配置内容来进行最大step之内的训练,到达Max_step自动停止训练
+    global_step = 0
+    while 1 == 1:
+        batch_inputs, batch_lables = sample.genbatch( inputs, lables, batch_size=config.batch_size)
+        #进行1个epoch的训练
+        for step in range(len(inputs)/batch_size):
+            feed_dict = { dnnmodel.inputs:batch_inputs, dnnmodel.label:batch_lables, dnnmodel.keep_prob:config.keep_prob }
+            with tf.Session() as sess:
+                global_step, logits, loss = sess.run(dnnmodel.global_step, dnnmodel.train_step, dnnmodel.loss, feed_dict=feed_dict)
+                if global_step % config.logfrequency == 0:
+                    #每间隔指定的频率打印日志并存储checkpoint文件
+                    logging.info('step [{0}] loss [{1}]'.format(global_step, loss))
+                    saver.save(sess, os.path.join(config.output_dir, "model.ckpt"), global_step=global_step)
+                if global_step >= config.Max_step:
+                    break
+        if global_step >= config.Max_step:
+            break
 
 if __name__ == '__main__':
     #train部分
-    inputs,lables = gendata()
-    with tf.Seesion() as initsess:
-        initsess.run(tf.global_variables_initializer())
-    with tf.Session() as sess:
-        feed_dict={ model.inputs:inputs, model.label:lables, model.keep_prob:0.8 }
-        sess.run(model.train_step,feed_dict=feed_dict)
+    train_model()
+
+
+
+
+
+
+
