bde221d467d96dfecbd431c0268e614c928f1214
liuysong <liuyong_song@126.com>
2018-10-22 16:27:13 +0800

    modify
    modify

--- model.py
+++ model.py
@@ -5,43 +5,57 @@ from __future__ import division
 from __future__ import print_function
 
 import tensorflow as tf
+import config
 
 slim = tf.contrib.slim
 
 
 # 实现模型的构建以及损失函数交叉熵的计算
 class Model():
-    def __init__(self, learning_rate):
+    def __init__(self, learning_rate, oridata_dim, embed_max):
         self.learning_rate = learning_rate
+        self.outunits = [1024, 128, 8]
+        self.oridata_dim = oridata_dim
+        self.embed_dim = config.embed_dim
+        self.embed_max = embed_max
+
+    def embeding(self, embed_inputs):
+        with tf.name_scope("embedding"):
+            embed_inputs = tf.cast(embed_inputs, tf.int32)
+            oridata_dim = self.oridata_dim
+            embed_matrix = tf.Variable(tf.random_uniform([self.embed_max, self.embed_dim], -1, 1), name="embed_matrix")
+            embed_layer = tf.nn.embedding_lookup(embed_matrix, embed_inputs, name="embed_layer")
+            embed_layer = tf.reshape(embed_layer, [-1, self.embed_dim * oridata_dim])
+        return embed_layer
 
     # 正态分布生成器,输入参数为:标准差standard deviation
-    def trunc_normal(stddev):
+    def trunc_normal(self, stddev):
         return tf.truncated_normal_initializer(stddev=stddev)
 
     # 实现了H_l()Composite function,此处暂时按照以下四个步骤构建，视需要和作用进行调整
-    def bn_act_conv_drp(current, num_outputs, scope='block'):
+    def bn_act_conv_drp(self, current, num_outputs, scope='block'):
         current = slim.batch_norm(current, scope=scope + '_bn')
         current = tf.nn.relu(current)
         current = slim.fully_connected(current, num_outputs, scope=scope + '_fully')
         current = slim.dropout(current, scope=scope + '_dropout')
         return current
 
-    def block(net, layers, growth, scope='block'):
+    def block(self, net, layers, growth, scope='block'):
         for idx in range(layers):
-            bottleneck = bn_act_conv_drp(net, 4 * growth,
+            bottleneck = self.bn_act_conv_drp(net, 4 * growth,
                                          scope=scope + '_botfully' + str(idx))
-            tmp = bn_act_conv_drp(bottleneck, growth,
+            tmp = self.bn_act_conv_drp(bottleneck, growth,
                                   scope=scope + '_tmpfullly' + str(idx))
             net = tf.concat(axis=1, values=[net, tmp])
         return net
 
     # 对于由全连接神经元构建的类densenet网络结构，由于denseblock会并联前几层的输出，所以会导致特征维数下降缓慢，所以需要transiton层实现特征降维
-    def transition(current, num_outputs, scope='trans'):
+    def transition(self, current, num_outputs, scope='trans'):
         current = slim.batch_norm(current, scope=scope + '_bn')
         current = slim.fully_connected(current, num_outputs, scope=scope + '_tsfully')
         return current
 
-    def densenet(self,inputs, final_endpoint='Predictions', num_classes=2, is_training=False,
+    def densenet(self, inputs, final_endpoint='Predictions', num_classes=2, is_training=False,
                  dropout_keep_prob=0.8,
                  scope='densenet'):
         '''
@@ -60,8 +74,7 @@ class Model():
         end_points = {}
 
         with tf.variable_scope(scope, 'DenseNet', [inputs, num_classes]):
-            with slim.arg_scope(self.bn_drp_scope(is_training=is_training,
-                                             keep_prob=dropout_keep_prob)) as ssc:
+            with slim.arg_scope(self.bn_drp_scope(is_training=is_training, keep_prob=dropout_keep_prob)) as ssc:
 
                 #
                 # batchsize * 2048
@@ -132,7 +145,7 @@ class Model():
 
         return logits, end_points
 
-    def bn_drp_scope(is_training=True, keep_prob=0.8):
+    def bn_drp_scope(self, is_training=True, keep_prob=0.8):
         keep_prob = keep_prob if is_training else 1
         with slim.arg_scope(
                 [slim.batch_norm],
@@ -149,9 +162,9 @@ class Model():
         self.keep_prob = tf.placeholder(tf.float32, name='keep_prob')
 
         with tf.variable_scope('builddensenet'):
-            logits, end_points = self.densenet(inputs= self.inputs)
+            logits, end_points = self.densenet(inputs=self.inputs)
 
-        #计算交叉熵损失函数
+        # 计算交叉熵损失函数
         with tf.variable_scope():
             labels = self.label
             loss = slim.losses.softmax_cross_entropy(logits, labels, weights=1.0)
@@ -159,16 +172,3 @@ class Model():
 
         with tf.variable_scope():
             self.train_step = tf.train.AdamOptimizer(self.learning_rate).minimize(self.loss)
-
-
-
-
-
-
-
-
-
-
-
-
-
